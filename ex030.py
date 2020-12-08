4ef25759b0eec3b019fe121d5a3f4266da4987f2
wwg0607 <wwg0607@jbnu.ac.kr>
2020-12-08 18:12:07 +0900

    Added file

---
+++ ex030.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+string = 'abcd'            # 문자열로 이루어진 자료형
+string.replace('b', 'B')   # replace를 사용해도 문자열이기 때문에 변경되지 않는다.
+print(string)              # 따라서 원래 자료 abcd 그대로 출력
+
+
+# In[ ]:
+
+
+
+
